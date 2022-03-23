08f07299d37f4430d509075358bbf46d72d07380
Hugo <karhou@intexnetworking.com>
2022-03-23 23:50:34 +0800

    1. remove group by for get all tables api

--- pages/api/table/index.js
+++ pages/api/table/index.js
@@ -7,14 +7,6 @@ export default async function handler(req, res) {
 
         const tables = await Table.find({});
 
-        let results = tables.reduce(function(results, table) {
-
-            results[table.chairs.length] = [...results[table.chairs.length] || [], table];
-
-            return results;
-
-          }, {});
-
-        return results;
+        return tables;
     });
 }
